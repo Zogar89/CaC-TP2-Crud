845d94a45eb0889043a2e7893e5e5fce99f16832
Zogar89 <gabrielalejandrogarcia@gmail.com>
2022-06-26 03:33:07 -0300

    conexion db

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+app=Flask(__name__)
+
+mysql=MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='tp2sistema'
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'aaaaaaaaaaaaaaaaaaaaaasd', 'asdasdas', 'asdsadasd')"
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    
+
+    return render_template('empleados/index.html')
+
+if __name__ == '__main__':
+    #DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(debug=True)
