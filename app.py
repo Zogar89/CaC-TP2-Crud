230b12bba45f9fb9eb3d6d70833980c33bdea391
Zogar89 <gabrielalejandrogarcia@gmail.com>
2022-06-30 02:55:13 -0300

    first create form upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 app=Flask(__name__)
@@ -26,6 +26,18 @@ def index():
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    nombre=request.form['txtNombre']
+    correo=request.form['txtCorreo']
+    foto=request.files['txtFoto']
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, '{}', '{}', '{}')".format(nombre, correo, foto.filename)
+    conn=mysql.connect()
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    return render_template('empleados/index.html')
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
