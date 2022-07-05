66057821f76751ab868d32ecee10a55f3ec6fa5a
Zogar89 <gabrielalejandrogarcia@gmail.com>
2022-07-04 23:13:57 -0300

    Delette routine

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from time import strftime
 from flask import Flask
 from flask import render_template, request
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app=Flask(__name__)
 
@@ -14,25 +16,38 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'aaaaaaaaaaaaaaaaaaaaaasd', 'asdasdas', 'asdsadasd')"
+    sql="SELECT * FROM `empleados`;"
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
+    empleados=cursor.fetchall()
     conn.commit()
-    
 
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados=empleados) 
 
 @app.route('/create')
 def create():
     return render_template('empleados/create.html')
+
+@app.route('/destroy/<int:id>')
+def destroy(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM 'empleados' WHERE id=%s")
 
 @app.route('/store', methods=['POST'])
 def storage():
     nombre=request.form['txtNombre']
     correo=request.form['txtCorreo']
     foto=request.files['txtFoto']
-    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, '{}', '{}', '{}')".format(nombre, correo, foto.filename)
+
+    now=datetime.now()
+    tiempo=now.strftime("%Y%D%M%H%S")
+
+    if foto.filename != '':
+        nuevoNombreFoto = tiempo + foto.filename
+        foto.save("uploads/"+nuevoNombreFoto)
+
+    sql="INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, '{}', '{}', '{}')".format(nombre, correo, nuevoNombreFoto)
     conn=mysql.connect()
     cursor=conn.cursor()
     cursor.execute(sql)
